68b440bfe44c642fd4bfb1b5909df9a5dcb41b49
hyeonjoonpark <pjjoon1379@gmail.com>
2023-12-24 18:26:16 +0900

    캘린더 스타일 수정

--- lib/ui/widget/calender.dart
+++ lib/ui/widget/calender.dart
@@ -21,53 +21,58 @@ class _CalendarState extends State<Calendar> {
       children: [
         Container(
           width: 0.8.sw,
-          child: TableCalendar(
-            calendarFormat: _calendarFormat,
-            focusedDay: _focusedDay,
-            firstDay: DateTime.utc(0000, 00, 00),
-            lastDay: DateTime.utc(2030, 12, 31),
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            headerStyle: const HeaderStyle(
-              formatButtonVisible: false,
+          child: Align(
+            alignment: Alignment.center,
+            child: TableCalendar(
+              calendarFormat: _calendarFormat,
+              focusedDay: _focusedDay,
+              firstDay: DateTime.utc(0000, 00, 00),
+              lastDay: DateTime.utc(2030, 12, 31),
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              headerStyle: const HeaderStyle(
+                formatButtonVisible: false,
+                titleTextStyle: TextStyle(
+                  color: SystemColors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              calendarStyle: const CalendarStyle(
+                selectedDecoration: BoxDecoration(
+                  color: Color(0xFFD7E5F5),
+                  shape: BoxShape.circle,
+                ),
+                selectedTextStyle: TextStyle(
+                  color: SystemColors.systemBlue,
+                ),
+                todayDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                ),
+                todayTextStyle: TextStyle(
+                  color: SystemColors.black,
+                ),
+                weekendTextStyle: TextStyle(
+                  color: SystemColors.black,
+                ),
+                outsideDaysVisible: true,
+                outsideTextStyle: TextStyle(
+                  color: SystemColors.systemGray,
+                ),
+              ),
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
             ),
-            calendarStyle: const CalendarStyle(
-              // 선택한 날짜의 focus 색상을 변경합니다.
-              selectedDecoration: BoxDecoration(
-                color: Color(0xFFD7E5F5),
-                shape: BoxShape.circle,
-              ),
-              selectedTextStyle: TextStyle(
-                color: SystemColors.systemBlue,
-              ),
-              // 현재 focus된 날짜의 색상을 변경합니다.
-              todayDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-              ),
-              todayTextStyle: TextStyle(
-                color: SystemColors.black,
-              ),
-              weekendTextStyle: TextStyle(
-                color: SystemColors.black,
-              ),
-              // 다른 월에 속하는 날짜의 색상을 변경합니다.
-              outsideDaysVisible: true, // 다른 월의 날짜를 숨기려면 true 대신 false로 설정하세요.
-              outsideTextStyle: TextStyle(
-                color: SystemColors.systemGray, // 변경할 색상을 지정하세요.
-              ),
-            ),
-            onFormatChanged: (format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              });
-            },
           ),
         ),
       ],
